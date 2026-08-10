50c1a6ad5f040f204639f531cb23c52f3d2e9e41
18july123-pixel <18july123@gmail.com>
2026-08-10 06:20:24 +0000

    Fix: use absolute import for config in google_client_manager to avoid relative import error

--- bot/google_client_manager.py
+++ bot/google_client_manager.py
@@ -13,7 +13,7 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from ..config import cfg
+from config import cfg
 
 
 @dataclass
